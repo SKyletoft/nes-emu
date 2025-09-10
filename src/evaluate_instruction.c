89fb4632b32c518088e938bbfcc689b85a61a230
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-10 03:50:04 +0200

    evaluate_instruction: Add template for first instruction

--- src/evaluate_instruction.c
+++ src/evaluate_instruction.c
@@ -1,5 +1,17 @@
 #include "inc/cpu.h"
 
+#include <stdint.h>
+
 int test() {
 	return 5;
+}
+
+void adc_immediate(Cpu *cpu, uint8_t val) {
+	uint16_t res = (uint16_t) cpu->a + (uint16_t) cpu->p.C + (uint16_t) val;
+
+	cpu->p.C = res > 255;
+	cpu->p.Z = 0 == (uint8_t) res;
+	cpu->p.V = ((res ^ (uint16_t) cpu->a) & (res ^ (uint16_t) val) & (uint16_t) 0x80) != 0;
+	cpu->p.N = (res & (1 >> 7)) >> 7;
+	cpu->a   = (uint8_t) res;
 }
