44cb334587fdbe4df055f8752df8b0fbadb8db75
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-10 03:17:21 +0200

    build/cpu: Specify which bindings

--- build.rs
+++ build.rs
@@ -12,6 +12,6 @@ fn main() {
 	// Write the bindings to the $OUT_DIR/bindings.rs file
 	let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
 	bindings
-		.write_to_file(out_path.join("bindings.rs"))
+		.write_to_file(out_path.join("cpubindings.rs"))
 		.expect("Couldn't write bindings!");
 }
