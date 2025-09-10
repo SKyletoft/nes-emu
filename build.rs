2d10931f7f11300c2201f3a29401fc244691c6cd
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-10 03:17:44 +0200

    build/evaluate-instruction: Add bindings to evaluate-instruction module

--- build.rs
+++ build.rs
@@ -14,4 +14,17 @@ fn main() {
 	bindings
 		.write_to_file(out_path.join("cpubindings.rs"))
 		.expect("Couldn't write bindings!");
+
+	// Generate bindings from the header file
+	let bindings = bindgen::Builder::default()
+		.header("src/evaluate-instruction.c")
+		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
+		.generate()
+		.expect("Unable to generate bindings");
+
+	// Write the bindings to the $OUT_DIR/bindings.rs file
+	let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+	bindings
+		.write_to_file(out_path.join("instbindings.rs"))
+		.expect("Couldn't write bindings!");
 }
