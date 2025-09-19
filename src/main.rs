3e2fb3e84f2f006fb350e7638b91e37fb5b8b528
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-19 18:34:02 +0200

    main: wait for input for each step + print status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cpu::P;
 use interpret::State;
 use nes_file::Mapper;
 
@@ -7,6 +8,26 @@ mod inst;
 mod interpret;
 mod nes_file;
 
+fn display(state: &State) {
+	let cpu::Cpu {a, x, y, s, p, pc, ..} = state.cpu;
+
+	let c = p.c();
+	let z = p.z();
+	let i = p.i();
+	let d = p.d();
+	let b = p.b();
+	let v = p.v();
+	let n = p.n();
+
+	let inst = state.next_inst();
+
+	println!("┌─CPU──────────────────────────┐");
+	println!("│ A:{a:02X} X:{x:02X} Y:{y:02X} SP:{s:02X} pc:{pc:04X} │");
+	println!("│ C:{c} Z:{z} I:{i} D:{d} B:{b} V:{v} N:{n}  │");
+	println!("└──────────────────────────────┘");
+	println!("Next: {inst:?}");
+}
+
 fn main() {
 	let path = std::env::args().nth(1).unwrap_or_else(|| "../non-free/SMB3.nes".into());
 	dbg!(&path);
@@ -14,7 +35,12 @@ fn main() {
 	let game = Mapper::try_from(buffer).unwrap();
 	let mut system_state = State::new(game);
 
+	let mut buf = String::new();
 	loop {
 		system_state = system_state.interpret();
+
+		display(&system_state);
+		buf.clear();
+		std::io::stdin().read_line(&mut buf).unwrap();
 	}
 }
