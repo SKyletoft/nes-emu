521d929aeb470dc8810b3142c8fe8ce544be3ccb
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-19 18:21:11 +0200

    Cargo.toml/build.rs/cpu/evaluate_instruction: Stop using bindgen

--- build.rs
+++ build.rs
@@ -1,43 +1,6 @@
 fn main() {
-	cpu_h();
-	evaluate_instruction_c();
-}
-
-fn cpu_h() {
-	// Tell cargo to invalidate the built crate whenever the header changes
-	println!("cargo:rerun-if-changed=inc/cpu.h");
-
-	// Generate bindings from the header file
-	let bindings = bindgen::Builder::default()
-		.header("inc/cpu.h")
-		.parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-		.generate()
-		.expect("Unable to generate bindings");
-
-	// Write the bindings to the $OUT_DIR/bindings.rs file
-	let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-	bindings
-		.write_to_file(out_path.join("cpubindings.rs"))
-		.expect("Couldn't write bindings!");
-}
-
-fn evaluate_instruction_c() {
 	// Tell cargo to invalidate the built crate whenever the header changes
 	println!("cargo:rerun-if-changed=src/evaluate_instruction.c");
-
-	// Generate bindings from the header file
-	let bindings = bindgen::Builder::default()
-		.header("src/evaluate_instruction.c")
-		.clang_arg("-I.")
-		.parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-		.generate()
-		.expect("Unable to generate bindings");
-
-	// Write the bindings to the $OUT_DIR/bindings.rs file
-	let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-	bindings
-		.write_to_file(out_path.join("instbindings.rs"))
-		.expect("Couldn't write bindings!");
 
 	let mut build = cc::Build::new();
 	build.file("src/evaluate_instruction.c");
--- src/cpu.rs
+++ src/cpu.rs
@@ -1,2 +1,68 @@
-#![allow(non_camel_case_types, non_snake_case, unused, non_upper_case_globals)]
-include!(concat!(env!("OUT_DIR"), "/cpubindings.rs"));
+// #![allow(non_camel_case_types, non_snake_case, unused, non_upper_case_globals)]
+// include!(concat!(env!("OUT_DIR"), "/cpubindings.rs"));
+
+use bitflags::bitflags;
+
+#[repr(transparent)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct P(u8);
+
+bitflags! {
+	impl P: u8 {
+		const C = 1 << 0;
+		const Z = 1 << 1;
+		const I = 1 << 2;
+		const D = 1 << 3;
+		const B = 1 << 4;
+		// unused bit 5
+		const V = 1 << 6;
+		const N = 1 << 7;
+	}
+}
+
+impl P {
+	pub fn new() -> Self {
+		let mut p = P(0);
+		p.0 |= 1 << 5;
+		p
+	}
+
+	pub fn c (&self) -> u8 {
+		self.contains(P::C) as u8
+	}
+
+	pub fn z (&self) -> u8 {
+		self.contains(P::Z) as u8
+	}
+
+	pub fn i (&self) -> u8 {
+		self.contains(P::I) as u8
+	}
+
+	pub fn d (&self) -> u8 {
+		self.contains(P::D) as u8
+	}
+
+	pub fn b (&self) -> u8 {
+		self.contains(P::B) as u8
+	}
+
+	pub fn v (&self) -> u8 {
+		self.contains(P::V) as u8
+	}
+
+	pub fn n (&self) -> u8 {
+		self.contains(P::N) as u8
+	}
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Cpu {
+	pub a: u8,
+	pub x: u8,
+	pub y: u8,
+	pub s: u8,
+	pub p: P,
+	pub pc: u16,
+}
