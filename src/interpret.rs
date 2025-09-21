c73146bfeac388209e1b3f5e4c7f268c145b4f97
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-21 05:20:20 +0200

    interpret: ppu-writer

--- src/interpret.rs
+++ src/interpret.rs
@@ -80,12 +80,29 @@ impl State {
 		inst.evaluate(self);
 	}
 
+	fn read_ppu(&self, adr: u16) -> u8 {
+		match adr % 8 {
+			0 => self.bus,
+			1 => self.bus,
+			2 => self.ppu.status.into(),
+			3 => self.bus,
+			4 => self.ppu.oam_data.into(),
+			5 => self.bus,
+			6 => self.bus,
+			7 => self.ppu.data.into(),
+			_ => unreachable!(),
+		}
+	}
+
+	fn write_ppu(&self, adr: u16, val: u8) {
+		todo!()
+	}
+
 	fn mem_pure(&self, adr: u16) -> u8 {
 		match adr {
 			0x0000..0x0800 => self.ram[adr as usize],
 			0x0800..0x2000 => self.ram[(adr % 2048) as usize],
-			0x2000..0x2008 => todo!(),
-			0x2008..0x4000 => todo!(),
+			0x2000..0x4000 => self.read_ppu(adr),
 			0x4000..0x4018 => todo!(),
 			0x4018..0x4020 => todo!(),
 			0x4020..=0xFFFF => self.rom.get_cpu(adr).expect("Invalid address for ROM"),
