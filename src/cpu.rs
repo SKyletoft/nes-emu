df695f08283d43f6ba755712df97f824e292cd04
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-25 15:31:09 +0200

    cpu: Use bitfields instead of bitflags

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,63 +1,29 @@
-// #![allow(non_camel_case_types, non_snake_case, unused, non_upper_case_globals)]
-// include!(concat!(env!("OUT_DIR"), "/cpubindings.rs"));
+use bitfields::bitfield;
+use bytemuck::Zeroable;
 
-use bitflags::bitflags;
-
-#[repr(transparent)]
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
-pub struct P(u8);
-
-bitflags! {
-	impl P: u8 {
-		const C = 1 << 0;
-		const Z = 1 << 1;
-		const I = 1 << 2;
-		const D = 1 << 3;
-		const B = 1 << 4;
-		// unused bit 5
-		const V = 1 << 6;
-		const N = 1 << 7;
-	}
-}
-
-impl P {
-	pub fn new() -> Self {
-		let mut p = P(0);
-		p.0 |= 1 << 5;
-		p
-	}
-
-	pub fn c(&self) -> u8 {
-		self.contains(P::C) as u8
-	}
-
-	pub fn z(&self) -> u8 {
-		self.contains(P::Z) as u8
-	}
-
-	pub fn i(&self) -> u8 {
-		self.contains(P::I) as u8
-	}
-
-	pub fn d(&self) -> u8 {
-		self.contains(P::D) as u8
-	}
-
-	pub fn b(&self) -> u8 {
-		self.contains(P::B) as u8
-	}
-
-	pub fn v(&self) -> u8 {
-		self.contains(P::V) as u8
-	}
-
-	pub fn n(&self) -> u8 {
-		self.contains(P::N) as u8
-	}
+#[bitfield(u8)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Zeroable)]
+pub struct P {
+	#[bits(1)]
+	c: bool,
+	#[bits(1)]
+	z: bool,
+	#[bits(1)]
+	i: bool,
+	#[bits(1)]
+	d: bool,
+	#[bits(1)]
+	b: bool,
+	#[bits(1)]
+	_unused: bool,
+	#[bits(1)]
+	v: bool,
+	#[bits(1)]
+	n: bool,
 }
 
 #[repr(C)]
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Zeroable)]
 pub struct Cpu {
 	pub a: u8,
 	pub x: u8,
