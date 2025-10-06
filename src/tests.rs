57fc8b5a273d0fe6fb3ee4445cdc6b5761652281
Samuel Kyletoft <samuel@kyletoft.se>
2025-10-06 02:31:34 +0200

    tests: Spam instructions

--- src/tests.rs
+++ src/tests.rs
@@ -873,6 +873,7 @@ macro_rules! make_log_test {
 					ours, line,
 					"Mismatch at line {i}\n ours: {ours}\n ref : {line}\n{debug_state}"
 				);
+				println!("{:X?}", state.next_inst());
 				state.next();
 			}
 		}
