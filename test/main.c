9ac7ad1905a6ec565e444a3f18f72a77cff5ce3e
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-12 23:26:31 +0100

    test: Load SMB1.nes

--- test/main.c
+++ test/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#include "interface.h"
+
+void bb_8000(State *state);
+
+State *new_state_from_file_name(const char *name);
+
 int main() {
-	printf("Hello!\n");
+	State *state = new_state_from_file_name("../non-free/SMB1.nes");
+	bb_8000(state);
 }
