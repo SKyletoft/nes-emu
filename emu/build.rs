abef5a11bc1a5f74c72e4cf547cd76dc82a20a19
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-16 03:29:16 +0100

    emu: Rebuild when libmario.a changes

--- emu/build.rs
+++ emu/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+	println!("cargo:rerun-if-changed={}/../nesc/src/libmario.a", env!("CARGO_MANIFEST_DIR"));
 	println!("cargo:rustc-link-search=native={}/../nesc/src", env!("CARGO_MANIFEST_DIR"));
 	println!("cargo:rustc-link-lib=static=mario");
 }
