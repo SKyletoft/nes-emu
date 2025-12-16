d498ad19ff00c5c133810e165360c2d97892e876
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-16 13:16:57 +0100

    nesc: Remove deadcode

--- nesc/src/main.rs
+++ nesc/src/main.rs
@@ -1,243 +1,11 @@
-#![allow(dead_code)]
-
 use std::{
-	collections::{BTreeSet, VecDeque},
-	io::{Read, Seek, SeekFrom, Write},
+	collections::VecDeque,
+	io::{Write, Read, Seek, SeekFrom},
 };
 
 use anyhow::{Result, anyhow, bail};
-use emu_core::{graphics, inst::Inst, interpret::State, nes_file::Mapper};
+use emu_core::{inst::Inst, nes_file::Mapper};
 use tempfile::NamedTempFile;
-
-#[derive(Debug)]
-struct Block {
-	starting_at: u16,
-	insts: Vec<Inst>,
-	then: u16,
-	otherwise: Option<u16>,
-}
-
-fn to_h<T: Write>(Block { starting_at, .. }: &Block, out: &mut T) -> Result<()> {
-	writeln!(out, "void bb_{starting_at:04X}(State *state);")?;
-	Ok(())
-}
-
-fn to_c<T: Write>(
-	Block {
-		starting_at,
-		insts,
-		then,
-		otherwise,
-	}: &Block,
-	out: &mut T,
-) -> Result<()> {
-	writeln!(out, "void bb_{starting_at:04X}(State *state) {{")?;
-
-	for inst in insts.iter() {
-		write!(out, "\t{}", inst.instruction_representation())?;
-	}
-
-	fn branch<T: Write>(out: &mut T, cond: &str, then: u16, other: u16) -> Result<()> {
-		writeln!(out, "\tif ({cond}) {{")?;
-		writeln!(out, "\t\t[[clang::musttail]] return bb_{then:04X}(state);")?;
-		writeln!(out, "\t}} else {{")?;
-		writeln!(out, "\t\t[[clang::musttail]] return bb_{other:04X}(state);")?;
-		writeln!(out, "\t}}")?;
-		Ok(())
-	}
-
-	match (insts.last().unwrap(), otherwise) {
-		(Inst::Bcc(_), Some(other)) => branch(out, "state->cpu.p.C == 0", *then, *other)?,
-		(Inst::Bcs(_), Some(other)) => branch(out, "state->cpu.p.C != 0", *then, *other)?,
-		(Inst::Bpl(_), Some(other)) => branch(out, "state->cpu.p.N == 0", *then, *other)?,
-		(Inst::Bmi(_), Some(other)) => branch(out, "state->cpu.p.N != 0", *then, *other)?,
-		(Inst::Bne(_), Some(other)) => branch(out, "state->cpu.p.Z == 0", *then, *other)?,
-		(Inst::Beq(_), Some(other)) => branch(out, "state->cpu.p.Z != 0", *then, *other)?,
-		(Inst::Bvc(_), Some(other)) => branch(out, "state->cpu.p.V == 0", *then, *other)?,
-		(Inst::Bvs(_), Some(other)) => branch(out, "state->cpu.p.V != 0", *then, *other)?,
-		(Inst::Jsr(_), Some(_)) => {
-			writeln!(out, "\t[[clang::musttail]] return bb_{then:04X}(state);")?
-		}
-		(
-			Inst::Stp
-			| Inst::Stp2
-			| Inst::Stp3
-			| Inst::Stp4
-			| Inst::Stp5
-			| Inst::Stp6
-			| Inst::Stp7
-			| Inst::Stp8
-			| Inst::Stp9
-			| Inst::Stp10
-			| Inst::Stp11
-			| Inst::Stp12,
-			None,
-		) => writeln!(out, "\t[[clang::musttail]] return return_stp(state);")?,
-		(Inst::JmpAbsolute(_) | Inst::JmpIndirect(_) | Inst::Rti | Inst::Rts, None) => {
-			writeln!(out, "\t[[clang::musttail]] return jump_table(state);")?
-		}
-		e => bail!("Invalid bb ({e:?})"),
-	}
-
-	write!(out, "}}\n\n")?;
-
-	Ok(())
-}
-
-fn c_helpers<T: Write>(blocks: &[Block], out: &mut T) -> Result<()> {
-	writeln!(out, "void jump_table(State *state) {{")?;
-	writeln!(out, "\tswitch (state->cpu.pc) {{")?;
-	for &Block { starting_at, .. } in blocks.iter() {
-		writeln!(
-			out,
-			"\tcase 0x{starting_at:04X}: [[clang::musttail]] return bb_{starting_at:04X}(state);"
-		)?;
-	}
-	writeln!(out, "\tdefault: {{")?;
-	writeln!(out, "\t\tprintf(\"Unknown block %X\\n\", state->cpu.pc);")?;
-	writeln!(out, "\t\texit(-1);")?;
-	writeln!(out, "\t}}")?;
-	writeln!(out, "\t}}")?;
-	writeln!(out, "}}\n")?;
-
-	writeln!(out, "void return_stp([[maybe_unused]] State *state) {{")?;
-	writeln!(
-		out,
-		"\tprintf(\"Unimplemented\\n%d %X\\n\", state->cpu.pc, state->cpu.pc);"
-	)?;
-	writeln!(out, "\texit(-1);")?;
-	writeln!(out, "}}\n")?;
-
-	Ok(())
-}
-
-fn find_blocks(rom: Box<Mapper>) -> Vec<Block> {
-	let mut blocks = Vec::new();
-	let mut queue = VecDeque::new();
-	queue.push_back(u16::from_le_bytes([
-		rom.get_cpu(0xFFFC).expect("Cannot read reset vector"),
-		rom.get_cpu(0xFFFD).expect("Cannot read reset vector (2)"),
-	]));
-	queue.push_back(u16::from_le_bytes([
-		rom.get_cpu(0xFFFA).expect("Cannot read interrupt vector"),
-		rom.get_cpu(0xFFFB)
-			.expect("Cannot read interrupt vector (2)"),
-	]));
-	let mut visited = BTreeSet::new();
-
-	let mut system_state = State::new(rom, graphics::new_bitmap());
-	while let Some(adr) = queue.pop_front() {
-		if visited.contains(&adr) || adr < 0x4020 {
-			continue;
-		}
-		visited.insert(adr);
-		system_state.cpu.pc = adr;
-
-		let mut block = Block {
-			starting_at: adr,
-			insts: Vec::new(),
-			then: 0,
-			otherwise: None,
-		};
-
-		while let inst = system_state.next_inst_pure()
-			&& !inst.ends_bb()
-		{
-			block.insts.push(inst);
-			system_state.next();
-		}
-
-		let inst = system_state.next_inst_pure();
-		block.insts.push(inst);
-		match inst {
-			Inst::Bcc(_)
-			| Inst::Bcs(_)
-			| Inst::Bmi(_)
-			| Inst::Bne(_)
-			| Inst::Beq(_)
-			| Inst::Bpl(_) => {
-				let pc = system_state.cpu.pc;
-				system_state.cpu.p.set_bits(0);
-				system_state.next();
-				let then = system_state.cpu.pc;
-				system_state.cpu.pc = pc;
-				system_state.cpu.p.set_bits(0b1111_1111);
-				system_state.next();
-				let otherwise = system_state.cpu.pc;
-
-				queue.push_back(otherwise);
-				queue.push_back(then);
-				block.then = then;
-				block.otherwise = Some(otherwise);
-			}
-			Inst::JmpAbsolute(_) => {
-				system_state.next();
-				queue.push_back(system_state.cpu.pc);
-			}
-			Inst::JmpIndirect(_) => {
-				// Todo: Figure out how to deal with this. This should always be an escape hatch for ACE
-			}
-			Inst::Jsr(fn_adr) => {
-				let then = fn_adr.as_u16();
-				let otherwise = system_state.cpu.pc.wrapping_add(inst.len() as _);
-				block.then = then;
-				block.otherwise = Some(otherwise);
-				queue.push_back(then);
-				queue.push_back(otherwise);
-			}
-			Inst::Rti => {}
-			Inst::Rts => {}
-			Inst::Stp
-			| Inst::Stp2
-			| Inst::Stp3
-			| Inst::Stp4
-			| Inst::Stp5
-			| Inst::Stp6
-			| Inst::Stp7
-			| Inst::Stp8
-			| Inst::Stp9
-			| Inst::Stp10
-			| Inst::Stp11
-			| Inst::Stp12 => {}
-			e => panic!("Not a valid basic block end: {e:X?}\n{block:#?}"),
-		}
-
-		blocks.push(block);
-	}
-
-	blocks.sort_unstable_by_key(|b| b.starting_at);
-
-	blocks
-}
-
-fn write_to_c(blocks: &[Block]) -> Result<NamedTempFile> {
-	let mut tmpfile = NamedTempFile::new()?;
-
-	writeln!(&mut tmpfile, "#include \"evaluate_instruction.c\"")?;
-	writeln!(&mut tmpfile, "#include <stdio.h>")?;
-	writeln!(&mut tmpfile, "#include <stdlib.h>")?;
-	writeln!(&mut tmpfile)?;
-
-	writeln!(&mut tmpfile, "void jump_table(State *state);")?;
-	writeln!(&mut tmpfile, "void return_stp(State *state);")?;
-
-	writeln!(&mut tmpfile)?;
-
-	for block in blocks.iter() {
-		to_h(block, &mut tmpfile)?;
-	}
-	writeln!(&mut tmpfile)?;
-
-	for block in blocks.iter() {
-		to_c(block, &mut tmpfile)?;
-	}
-
-	c_helpers(blocks, &mut tmpfile)?;
-
-	tmpfile.seek(SeekFrom::Start(0))?;
-
-	Ok(tmpfile)
-}
 
 fn write_to_switch(rom: &Mapper) -> Result<NamedTempFile> {
 	let mut tmpfile = NamedTempFile::new()?;
@@ -449,10 +217,6 @@ fn main() -> Result<()> {
 		"Blocks are currently identified exclusively by address"
 	);
 
-	// let blocks = find_blocks(rom);
-	// let mut c = write_to_c(&blocks)?;
-	// c.disable_cleanup(true);
-
 	let mut c = write_to_switch(&rom)?;
 	c.disable_cleanup(true);
 
@@ -480,7 +244,7 @@ fn main() -> Result<()> {
 		.arg(concat!(env!("CARGO_MANIFEST_DIR"), "/../emu-core/src"))
 		.args(["-o", "mario.o"])
 		// .args(["-D", "STATIC_INLINE=[[clang::always_inline]] static inline"])
-		.args(["-D", "STATIC_INLINE=[[clang::noinline]]"])
+		// .args(["-D", "STATIC_INLINE=[[clang::noinline]]"])
 		.output()?;
 	println!("{}", String::from_utf8(cc_output.stdout)?);
 	eprintln!("{}", String::from_utf8(cc_output.stderr)?);
