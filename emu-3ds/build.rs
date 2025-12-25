45f523eb9a9c46317601f0f32df539fdd3537f74
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-25 16:42:26 +0100

    emu-3ds: Formatting

--- emu-3ds/build.rs
+++ emu-3ds/build.rs
@@ -1,5 +1,11 @@
 fn main() {
-	println!("cargo:rerun-if-changed={}/../nesc/src/libmario.a", env!("CARGO_MANIFEST_DIR"));
-	println!("cargo:rustc-link-search=native={}/../nesc/src", env!("CARGO_MANIFEST_DIR"));
+	println!(
+		"cargo:rerun-if-changed={}/../nesc/src/libmario.a",
+		env!("CARGO_MANIFEST_DIR")
+	);
+	println!(
+		"cargo:rustc-link-search=native={}/../nesc/src",
+		env!("CARGO_MANIFEST_DIR")
+	);
 	println!("cargo:rustc-link-lib=static=mario");
 }
