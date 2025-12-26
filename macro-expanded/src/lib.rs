11a75bbc1bb3cd21383baa6b5d1eb14641a1675a
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-26 17:43:17 +0100

    macro-expanded: Build nestest instead

--- macro-expanded/src/lib.rs
+++ macro-expanded/src/lib.rs
@@ -1,4 +1,5 @@
 use emu_core::{evaluate_instruction::*, interpret::State, nrom256::NROM256};
 use nesc_macro::compile_nes_to_rust;
 
-compile_nes_to_rust!("../non-free/SMB1.nes");
+// compile_nes_to_rust!("../non-free/SMB1.nes");
+compile_nes_to_rust!("../non-free/nestest.nes");
