a248b4f30e26803d07e9c1b035f530ffe8ad7b91
Samuel Kyletoft <samuel@kyletoft.se>
2026-01-02 02:33:50 +0100

    emu-core/cpu: remove default tags

--- emu-core/src/cpu.rs
+++ emu-core/src/cpu.rs
@@ -4,21 +4,15 @@ use bytemuck::Zeroable;
 #[bitfield(u8)]
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Zeroable)]
 pub struct P {
-	#[bits(1)]
 	c: bool,
-	#[bits(1)]
 	z: bool,
-	#[bits(1, default = true)]
+	#[bits(default = true)]
 	i: bool,
-	#[bits(1)]
 	d: bool,
-	#[bits(1)]
 	b: bool,
-	#[bits(1, default = true, access = ro)]
+	#[bits(default = true, access = ro)]
 	u: bool,
-	#[bits(1)]
 	v: bool,
-	#[bits(1)]
 	n: bool,
 }
 
