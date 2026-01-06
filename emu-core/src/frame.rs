41956d4b0bd0fcd5485df0184132bed9db245a66
Samuel Kyletoft <samuel@kyletoft.se>
2026-01-06 22:39:00 +0100

    emu-core: Correct coordinate order in frame buffer trait

--- emu-core/src/frame.rs
+++ emu-core/src/frame.rs
@@ -1,6 +1,6 @@
 use crate::ppu::NesColour;
 
 pub trait NesFramebuffer {
-	fn set(&mut self, x: usize, y: usize, col: NesColour);
+	fn set(&mut self, y: usize, x: usize, col: NesColour);
 	fn swap(&mut self);
 }
