4cb3675ceb2c1717ab5a6a05b9878c12da2293a1
Samuel Kyletoft <samuel@kyletoft.se>
2026-01-12 06:03:57 +0100

    mapper: Add get_bg_pixel to Mapper interface (for cachable backgrounds)

--- emu-core/src/mapper.rs
+++ emu-core/src/mapper.rs
@@ -1,6 +1,9 @@
 use bitfields::bitfield;
 
-use crate::ppu::Ppu;
+use crate::{
+	ppu::{NesColour, Ppu},
+	unsafe_assert, unsafe_unreachable,
+};
 
 pub trait Mapper {
 	fn get_cpu(&self, adr: u16) -> Option<u8>;
@@ -8,6 +11,31 @@ pub trait Mapper {
 	fn get_ppu(&self, adr: u16, ppu: &Ppu) -> Option<u8>;
 	fn set_ppu(&mut self, adr: u16, ppu: &mut Ppu, val: u8) -> Option<()>;
 	fn get_palette_index(&self, half: bool, tile: u8, y: u8, x: u8) -> u8;
+
+	fn get_bg_pixel(
+		&self,
+		tilemap_x: i16,
+		tilemap_y: i16,
+		ppu: &Ppu,
+		palettes: &[[NesColour; 4]; 8],
+	) -> Option<NesColour>
+	where
+		Self: Sized,
+	{
+		unsafe { unsafe_assert!((0..512).contains(&tilemap_x)) };
+		unsafe { unsafe_assert!((0..480).contains(&tilemap_y)) };
+
+		let Some(attribute) =
+			crate::interpret::calculate_attribute_bits(tilemap_x, tilemap_y, self, ppu)
+				.nth(tilemap_x as usize % 8)
+		else { unsafe { unsafe_unreachable!() } };
+		let Some(tile) =
+			crate::interpret::calculate_tile_palette_index(tilemap_x, tilemap_y, self, ppu)
+				.nth(tilemap_x as usize % 8)
+		else { unsafe { unsafe_unreachable!() } };
+
+		crate::interpret::calculate_background_colour(tile, attribute, palettes)
+	}
 }
 
 #[bitfield(u16)]
