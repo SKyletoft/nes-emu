f3a9cc24dac371a72d1b6a00e431f7e21e340e19
Samuel Kyletoft <samuel@kyletoft.se>
2026-02-18 11:27:10 +0100

    citro2d_framebuffer: Factor out tile and sprite updating methods

--- frontend/src/citro2d_framebuffer.rs
+++ frontend/src/citro2d_framebuffer.rs
@@ -63,60 +63,72 @@ impl<'a> Citro2DFramebuffer<'a> {
 			hide_right,
 		})
 	}
+
+	fn update_tile<M: Mapper>(&mut self, m: &M, ppu: &Ppu, x: usize, y: usize, x_offset: usize) {
+		let mut buffer = [[Rgba5551::TRANSPARENT; 8]; 8];
+		let bg = if x_offset == 0 {
+			&mut self.bg1
+		} else {
+			&mut self.bg2
+		};
+		for (col, pixel) in (0..8)
+			.flat_map(move |dy| (0..8).map(move |dx| (dx, dy)))
+			.map(|(dx, dy)| {
+				nes_colour_to_rgba5551(m.get_bg_pixel(
+					(x * 8 + dx + x_offset) as i16,
+					(y * 8 + dy) as i16,
+					ppu,
+					&ppu.palettes,
+				))
+			})
+			.zip(buffer.iter_mut().flat_map(|l| l.iter_mut()))
+		{
+			*pixel = col;
+		}
+		bg.texture_mut()
+			.unwrap()
+			.swizzle_and_update_tile(buffer, y as _, x as _);
+	}
+
+	fn update_sprite<M: Mapper>(&mut self, m: &M, ppu: &Ppu, idx: usize) {
+		let mut buffer = [[Rgba5551::TRANSPARENT; 8]; 8];
+		let data = m.get_sprite_pixels(idx, ppu);
+		for (pixel, col) in buffer
+			.iter_mut()
+			.flat_map(|xs: &mut [Rgba5551; 8]| xs.iter_mut())
+			.zip(data)
+		{
+			*pixel = nes_colour_to_rgba5551(col);
+		}
+		self.sprites[idx]
+			.texture_mut()
+			.unwrap()
+			.swizzle_and_update_tile(buffer, 0, 0);
+	}
 }
 
 impl NesFramebuffer for Citro2DFramebuffer<'_> {
 	fn render<M: Mapper>(&mut self, m: &M, ppu: &Ppu, lines: &[(i16, i16); 240]) {
-		let mut buffer = [[Rgba5551::TRANSPARENT; 8]; 8];
-
 		let (dirty_sprites, dirty_tiles) = m.dirty_tiles();
 
-		for ((bg, dirty_tiles), x_offset) in [&mut self.bg1, &mut self.bg2]
-			.into_iter()
-			.zip(dirty_tiles.into_iter())
-			.zip([0, 256].into_iter())
-		{
+		for (dirty_tiles, x_offset) in dirty_tiles.into_iter().zip([0, 256].into_iter()) {
 			for (x, y) in dirty_tiles.iter().enumerate().flat_map(|(x, row)| {
 				row.iter()
 					.enumerate()
 					.filter(|(_, b)| **b)
 					.map(move |(y, _)| (x, y))
 			}) {
-				for (col, pixel) in (0..8)
-					.flat_map(move |dy| (0..8).map(move |dx| (dx, dy)))
-					.map(|(dx, dy)| {
-						nes_colour_to_rgba5551(m.get_bg_pixel(
-							(x * 8 + dx + x_offset) as i16,
-							(y * 8 + dy) as i16,
-							ppu,
-							&ppu.palettes,
-						))
-					})
-					.zip(buffer.iter_mut().flat_map(|l| l.iter_mut()))
-				{
-					*pixel = col;
-				}
-				bg.texture_mut()
-					.unwrap()
-					.swizzle_and_update_tile(buffer, y as _, x as _);
+				self.update_tile(m, ppu, x, y, x_offset);
 			}
 		}
 
-		for ((idx, sprite), dirty) in self.sprites.iter_mut().enumerate().zip(dirty_sprites) {
+		for (idx, dirty) in dirty_sprites.iter().copied().enumerate() {
 			if dirty {
-				let data = m.get_sprite_pixels(idx, ppu);
-				for (pixel, col) in buffer
-					.iter_mut()
-					.flat_map(|xs: &mut [Rgba5551; 8]| xs.iter_mut())
-					.zip(data)
-				{
-					*pixel = nes_colour_to_rgba5551(col);
-				}
-				sprite
-					.texture_mut()
-					.unwrap()
-					.swizzle_and_update_tile(buffer, 0, 0);
+				self.update_sprite(m, ppu, idx);
 			}
+		}
+
+		for (idx, sprite) in self.sprites.iter_mut().enumerate() {
 			sprite.set_depth(if ppu.oam[idx].attr.priority() {
 				0.1
 			} else {
