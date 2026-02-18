9687fe5e9e52d243d4a1f5df07b0799734b0e21c
Samuel Kyletoft <samuel@kyletoft.se>
2026-02-18 10:17:18 +0100

    emu-core: Dummy framebuffer type

--- emu-core/src/frame.rs
+++ emu-core/src/frame.rs
@@ -3,3 +3,10 @@ use crate::{mapper::Mapper, ppu::Ppu};
 pub trait NesFramebuffer {
 	fn render<M: Mapper>(&mut self, m: &M, ppu: &Ppu, lines: &[(i16, i16); 240]);
 }
+
+#[derive(Clone, Debug)]
+pub struct NoFramebuffer;
+
+impl NesFramebuffer for NoFramebuffer {
+	fn render<M: Mapper>(&mut self, _: &M, _: &Ppu, _: &[(i16, i16); 240]) {}
+}
